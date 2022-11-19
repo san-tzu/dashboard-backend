c6ebb504da89de03fbadcd19daf406a1ba6ae958
Sahan Thenuwara <t.a.s.sanjith@gmail.com>
2022-11-19 06:09:23 +0530

    db connection

--- db.js
+++ db.js
@@ -4,15 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 
-// if (process.env.NODE_ENV === "development") {
-//   // Local Database
-//   const db = process.env.LOCAL_DB;
-// } else {
-//   // Atlas Database
-//   const db = process.env.MONGODB_URI;
-// }
-
-  const db = process.env.MONGODB_URI;
+if (process.env.NODE_ENV == "development") {
+  // Local Database
+  db = process.env.LOCAL_DB;
+} else {
+  // Atlas Database
+  db = process.env.MONGODB_URI;
+}
+console.log(process.env.NODE_ENV, process.env.LOCAL_DB)
+  // const db = process.env.MONGODB_URI;
   // const db = process.env.LOCAL_DB;
 
 mongoose.connect(db, {
